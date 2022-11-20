f84b8cf3bdb4b13d641f8a535157700ca02d904c
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-20 10:01:34 -0300

    create addClass form

--- src/main/java/com/laiszig/learners_academy/HelloServlet.java
+++ src/main/java/com/laiszig/learners_academy/HelloServlet.java
@@ -2,39 +2,27 @@ package com.laiszig.learners_academy;
 
 import java.io.*;
 
-import com.laiszig.learners_academy.config.HibernateUtil;
 import com.laiszig.learners_academy.entity.CourseClass;
 import com.laiszig.learners_academy.service.ClassService;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "class", value = "/class")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        String name = request.getParameter("name");
 
         CourseClass courseClass = new CourseClass();
-        courseClass.setName("Math");
+        courseClass.setName(name);
 
         ClassService classService = new ClassService();
         classService.save(courseClass);
 
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
     }
 
     public void destroy() {
---
+++ src/main/webapp/addClass.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Course Class</title>
+</head>
+<body>
+<h1><%= "Course Class" %>
+</h1>
+<br/>
+<form method = "post"  action = "class">
+    Name: <input type = "text" name = "name"/>
+    <br>
+    <input type = "submit" value = "Login"/>
+</form>
+</body>
+</html>
